4875161dcdcb4c05fa44421919638b49fbfd3944
FelipePinheiroFerreira <202501212905@alunos.estacio.br>
2025-04-17 10:16:03 +0000

    Leitura dos dados da Carta 1

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -18,6 +18,29 @@ int main() {
      float area2;
      float pib2;
      int pontosTuristicos2;
-     
+
+     // Leitura dos dados da Carta 1
+    printf("Cadastro da Carta 1:\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilhões): ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos1);
     return 0;
 }
